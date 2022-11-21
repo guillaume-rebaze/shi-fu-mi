54fc41b504ed9c54de6789e3abffa082e291da0a
guillaume34110 <gaillard.guillaum@gmail.com>
2022-11-21 14:43:03 +0100

    update

--- src/Logic.ts
+++ src/Logic.ts
@@ -1,56 +1,30 @@
+import "@effect-ts/node/Modules/Traced"
+
 import * as T from "@effect-ts/core/Effect"
 import { pipe } from "@effect-ts/core/Function"
-import { tag } from "@effect-ts/core/Has"
-
-
-interface ConsoleService {
-    log: (message: string) => T.Effect<unknown, never, void>;
-}
-
-interface RandomService {
-    rand: T.Effect<unknown, never, number>
-}
-
-const consoleService = tag<ConsoleService>()
-const randomService = tag<RandomService>()
-
-const log = (message: string) => T.accessService(consoleService)((_) => _.log(message))
-const rand = T.accessServiceM(randomService)((_) => _.rand)
-
-export function logResult(name: string) {
-    return log(`result : ${name}`)
-}
-
-
-class BadRandomValue {
-    readonly _tag = "BadRandomValue"
-    constructor(readonly n: number) { }
-}
-
-const program = T.gen(function* (_) {
-
-    const value = yield* _(rand)
-
-    if (value > 0.5) {
-        yield* _(T.fail(new BadRandomValue(value)))
-    } else {
-        yield* _(log(`got : ${value}`))
-    }
-
-})
-
 
 export const pp = () => pipe(
-    program,
-    T.provideService(consoleService)({
-        log: (message) => T.effectAsync(() => {
-            console.log(message)
-        })
-    }),
-    T.provideService(randomService)({
-        rand: T.(() => {
-            return Math.random()
-        })
-    }),
-    T.runPromise
+  T.succeed(1),
+  T.chain((n) => {
+    return T.succeed(n + 1)
+  }),
+  T.chain((n) => {
+    return T.succeed(n + 1)
+  }),
+  T.chain((n) => {
+    return T.succeed(n + 1)
+  }),
+  T.tap((n) => {
+    return T.fail(`(${n})`)
+  }),
+  T.catchAll(function handle(n) {
+    return T.succeed(n)
+  }),
+  T.chain((n) => {
+    return T.fail(`error: ${n}`)
+  }),
+  T.chain(() => T.succeed(0)),
+  T.result,
+  T.chain(T.done),
+  T.runPromise
 )
